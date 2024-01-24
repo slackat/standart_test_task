c4dda05d99269048713e73a7c7e75c75831f3b57
Vyacheslav Kataev <slavakataev27@gmail.com>
2024-01-25 03:17:31 +0500

    Quick fix and minor updates

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from utils import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import backref
 
 class Requisites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,8 +9,11 @@ class Requisites(db.Model):
     owner_name = db.Column(db.String(100))
     phone_number = db.Column(db.String(100))
     value_limit = db.Column(db.Integer)
+    payment_requests = db.relationship('PaymentRequests', backref=backref('requisites', cascade="all,delete"), lazy=True)
+
 
 class PaymentRequests(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Integer)
     status = db.Column(db.String(100))
+    requisites_id = db.Column(db.Integer, ForeignKey('requisites.id'), nullable=True)
--- app/routes.py
+++ app/routes.py
@@ -6,27 +6,25 @@ from utils import db
 app_routes = Blueprint('app_routes', __name__)
 
 
-@app_routes.route('/')
+@app_routes.route("/")
 def payment_requests():
-    requests = PaymentRequests.query.all()
-    requisites = Requisites.query.all()
+    joined_data = db.session.query(PaymentRequests, Requisites).outerjoin(Requisites,
+                                                                     PaymentRequests.id == Requisites.id
+                                                                     ).all()
 
     data = {}
 
-    for request in requests:
-        data[request.id] = {
-            'ID заявки': request.id,
-            'Сумма': request.amount,
-            'Статус': request.status,
+    for payment_request, requisite in joined_data:
+        data[payment_request.id] = {
+            'ID заявки': payment_request.id,
+            'Сумма': payment_request.amount,
+            'Статус': payment_request.status,
+            'Тип оплаты': requisite.payment_type if requisite else '',
+            'Тип аккаунта': requisite.account_type if requisite else '',
+            'Имя': requisite.owner_name if requisite else '',
+            'Номер телефона': requisite.phone_number if requisite else '',
+            'Лимит': requisite.value_limit if requisite else ''
         }
-
-    for requisite in requisites:
-        if requisite.id in data:
-            data[requisite.id]['Тип оплаты'] = requisite.payment_type
-            data[requisite.id]['Тип аккаунта'] = requisite.account_type
-            data[requisite.id]['Имя'] = requisite.owner_name
-            data[requisite.id]['Номер телефона'] = requisite.phone_number
-            data[requisite.id]['Лимит'] = requisite.value_limit
 
     table_data = list(data.values())
 
@@ -50,10 +48,9 @@ def requisites():
     }
 
     if search_field not in field_mapping:
-        return "Ничего не найдено"
+        return render_template('requisites.html')
 
     filters = [field_mapping[search_field].ilike(f'%{search_term}%')]
     requisites = Requisites.query.filter(or_(*filters)).order_by(db.text(f"{column_to_sort} {order_by}")).all()
 
     return render_template('requisites.html', requisites=requisites, column_to_sort=column_to_sort, order_by=order_by, search_term=search_term)
-
