0a5c689d2a06562198659d65aa38a69c99a2137b
Vyacheslav Kataev <slavakataev27@gmail.com>
2024-01-25 05:07:29 +0500

    Add seeder as optional function

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from utils import app
 from utils.seed import seed_database
 from routes import app_routes
+import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'seeder':
+        seed_database()
+        print("123")
     app.register_blueprint(app_routes)
-    seed_database()
     app.run()
