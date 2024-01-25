97d736b5ebb0939d92f55d7cbb2f24510b7c46db
Vyacheslav Kataev <slavakataev27@gmail.com>
2024-01-25 22:25:08 +0500

    Cleanup and add comments

--- app/api/invoices.py
+++ app/api/invoices.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify, request
-from flask_restful import Api, Resource, reqparse
+from flask import Blueprint, request
+from flask_restful import Api, Resource
 from models import Requisites, PaymentRequests
 
 invoices_bp = Blueprint('invoices', __name__, url_prefix='/api')
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -10,14 +10,17 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_swagger_ui import get_swaggerui_blueprint
 
+"""
+Configuring Flask app
+"""
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['TESTING'] = False
 app.config['PERMANENT_SESSION_LIFETIME'] = 60*60*24*30
 
-
-
-
+"""
+Configuring flask-swagger-ui
+"""
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
 
@@ -29,23 +32,22 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     }
 )
 
-
-
-
-
-
-
+"""
+Configuring database connection
+"""
 load_dotenv()
 POSTGRES_LOGIN = os.getenv('POSTGRES_LOGIN')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_ADDRESS = os.getenv('POSTGRES_ADDRESS')
 POSTGRES_DATABASE = os.getenv('POSTGRES_DATABASE')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_LOGIN}:{POSTGRES_PASSWORD}@{POSTGRES_ADDRESS}/{POSTGRES_DATABASE}'
+db = SQLAlchemy()
+db.init_app(app)
 
+"""
+Configuring Bcrypt and LoginManager
+"""
 bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'app_routes.login'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_LOGIN}:{POSTGRES_PASSWORD}@{POSTGRES_ADDRESS}/{POSTGRES_DATABASE}'
-db = SQLAlchemy()
-db.init_app(app)
